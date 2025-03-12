c1ea13311172b94a9db1dc3d60602968bb115826
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-03-12 02:48:12 -0300

    Criação do projeto

---
+++ src/main/java/com/empresa/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
