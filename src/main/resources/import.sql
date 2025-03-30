4202f2c8ea7f72630cdc379152d0d11d1b9e3c0c
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-03-30 15:39:25 -0300

    Fornecedor: schema.sql e import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+-- Inserindo dados na tabela Usuario:
+INSERT INTO USUARIO (id, nome, email, senha) VALUES ('1', 'Usuario A', 'usuarioa@example.com', 'senha123');
+INSERT INTO USUARIO (id, nome, email, senha) VALUES ('2', 'Usuario B', 'usuariob@example.com', 'senha456');
+INSERT INTO USUARIO (id, nome, email, senha) VALUES ('3', 'Usuario C', 'usuarioc@example.com', 'senha789');
+INSERT INTO USUARIO (id, nome, email, senha) VALUES ('4', 'Usuario D', 'usuariod@example.com', 'senha101');
+INSERT INTO USUARIO (id, nome, email, senha) VALUES ('5', 'Usuario E', 'usuarioe@example.com', 'senha202');
+
 -- Inserindo dados na tabela Fornecedor:
 INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'Fornecedor A', '31999999999', 'Rua A, 123');
 INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440001', 'Fornecedor B', '11888888888', 'Rua B, 456');
