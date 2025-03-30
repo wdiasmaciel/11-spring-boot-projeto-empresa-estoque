e2ce191f1ce3199c4b4f144a8b2b70baaeb8146c
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-03-30 02:57:02 -0300

    Fornecedor: arquivo import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- Inserindo dados na tabela Fornecedor:
+INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'Fornecedor A', '31999999999', 'Rua A, 123');
+INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440001', 'Fornecedor B', '11888888888', 'Rua B, 456');
+INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440002', 'Fornecedor C', '21777777777', 'Rua C, 789');
+INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440003', 'Fornecedor D', '48666666666', 'Rua D, 101');
+INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440004', 'Fornecedor E', '81555555555', 'Rua E, 202');
