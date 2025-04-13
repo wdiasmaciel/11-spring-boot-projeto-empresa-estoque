83c274fdd7df566637a452a3e8b0cb365aca2b97
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-04-12 23:49:40 -0300

    FornecedorMapper

--- src/main/java/com/empresa/app/controllers/FornecedorController.java
+++ src/main/java/com/empresa/app/controllers/FornecedorController.java
@@ -4,10 +4,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import com.empresa.app.dtos.FornecedorRequestDto;
