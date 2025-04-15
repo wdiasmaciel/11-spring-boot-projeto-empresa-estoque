17fdd16609f10208bb63307110aed65123c018e2
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-04-15 01:20:31 -0300

    Filial

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,3 +11,11 @@ INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d
 INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440002', 'Fornecedor C', '21777777777', 'Rua C, 789');
 INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440003', 'Fornecedor D', '48666666666', 'Rua D, 101');
 INSERT INTO FORNECEDOR (id, nome, telefone, endereco) VALUES ('550e8400-e29b-41d4-a716-446655440004', 'Fornecedor E', '81555555555', 'Rua E, 202');
+
+-- Inserindo dados na tabela Filial:
+INSERT INTO FILIAL (cnpj, nome, telefone, endereco) VALUES ('11111111000101', 'Filial 1', '11444444444', 'Avenida 1, 100');
+INSERT INTO FILIAL (cnpj, nome, telefone, endereco) VALUES ('22222222000102', 'Filial 2', '11333333333', 'Avenida 2, 200');
+INSERT INTO FILIAL (cnpj, nome, telefone, endereco) VALUES ('33333333000103', 'Filial 3', '11222222222', 'Avenida 3, 300');
+INSERT INTO FILIAL (cnpj, nome, telefone, endereco) VALUES ('44444444000104', 'Filial 4', '11111111111', 'Avenida 4, 400');
+INSERT INTO FILIAL (cnpj, nome, telefone, endereco) VALUES ('55555555000105', 'Filial 5', '11000000000', 'Avenida 5, 500');
+
