215904784e9e48f0d8a8210609b3c3951adefe66
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-04-19 01:37:59 -0300

    Service: Identificacao.

--- src/main/java/com/empresa/app/services/IdentificacaoService.java
+++ src/main/java/com/empresa/app/services/IdentificacaoService.java
@@ -7,10 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
-import com.empresa.app.dtos.IdentificacaoRequestDto;
-import com.empresa.app.dtos.IdentificacaoResponseDto;
+import com.empresa.app.dtos.IdentificacaoDto;
+import com.empresa.app.dtos.ProdutoResponseDto;
 import com.empresa.app.models.IdentificacaoModel;
+import com.empresa.app.models.ProdutoModel;
 import com.empresa.app.mappers.IdentificacaoMapper;
+import com.empresa.app.mappers.ProdutoMapper;
 import com.empresa.app.repositories.IdentificacaoRepository;
 
 @Service
@@ -24,53 +26,72 @@ public class IdentificacaoService {
     }
 
     @Transactional(readOnly = true)
-    public List<IdentificacaoResponseDto> findAll() {
+    public List<IdentificacaoDto> findAll() {
         List<IdentificacaoModel> listaIdentificacaoModel = identificacaoRepository.findAll();
         return listaIdentificacaoModel.stream()
-                .map(IdentificacaoMapper::toResponseDto) // Converte cada IdentificacaoModel para IdentificacaoResponseDto.
-                                                   // IdentificacaoMapper::toResponseDto é uma "method reference".
+                .map(IdentificacaoMapper::toDto) // Converte cada IdentificacaoModel para IdentificacaoDto.
+                                                 // IdentificacaoMapper::toDto é uma "method reference".
                 .toList();
     }
 
     @Transactional(readOnly = true)
-    public IdentificacaoResponseDto findById(UUID id) {
-        IdentificacaoModel IdentificacaoModel = IdentificacaoRepository.findById(id).orElse(null);
+    public IdentificacaoDto findById(ProdutoResponseDto produtoResponseDto) {
+        ProdutoModel produtoModel = ProdutoMapper.toModel(produtoResponseDto);
+        IdentificacaoModel identificacaoModel = identificacaoRepository.findById(produtoModel).orElse(null);
 
-        if (IdentificacaoModel == null)
+        if (identificacaoModel == null)
             return null;
 
-        return IdentificacaoMapper.toResponseDto(IdentificacaoModel);
+        return IdentificacaoMapper.toDto(identificacaoModel);
+    }
+
+    @Transactional(readOnly = true)
+    public IdentificacaoDto findById(UUID id) {
+        IdentificacaoModel identificacaoModel = identificacaoRepository.findByProdutoModelId(id).orElse(null);
+
+        if (identificacaoModel == null)
+            return null;
+
+        return IdentificacaoMapper.toDto(identificacaoModel);
     }
 
     @Transactional
-    public IdentificacaoResponseDto save(IdentificacaoRequestDto fornecedorRequestDto) {
-        IdentificacaoModel IdentificacaoModel = IdentificacaoRepository.save(IdentificacaoMapper.toModel(fornecedorRequestDto));
-        return IdentificacaoMapper.toResponseDto(IdentificacaoModel);
+    public IdentificacaoDto save(IdentificacaoDto identificacaoDto) {
+        IdentificacaoModel identificacaoModel = identificacaoRepository.save(IdentificacaoMapper.toModel(identificacaoDto));
+        return IdentificacaoMapper.toDto(identificacaoModel);
     }
-
+    
     @Transactional
-    public IdentificacaoResponseDto save(IdentificacaoResponseDto IdentificacaoResponseDto) {
-        IdentificacaoModel IdentificacaoModel = IdentificacaoRepository.save(IdentificacaoMapper.toModel(IdentificacaoResponseDto));
-        return IdentificacaoMapper.toResponseDto(IdentificacaoModel);
-    }
-
-    @Transactional
-    public IdentificacaoResponseDto update(UUID id, IdentificacaoRequestDto identificacaoRequestDtoComAtualizacao) {
-        IdentificacaoModel IdentificacaoModel = IdentificacaoRepository.findById(id).orElse(null);
+    public IdentificacaoDto update(ProdutoResponseDto produtoResponseDto, IdentificacaoDto identificacaoDtoComAtualizacao) {
+        ProdutoModel produtoModel = ProdutoMapper.toModel(produtoResponseDto);
+        IdentificacaoModel IdentificacaoModel = identificacaoRepository.findById(produtoModel).orElse(null);
         if (IdentificacaoModel != null) {
-            IdentificacaoModel.setNome(identificacaoRequestDtoComAtualizacao.getNome());
-            return IdentificacaoMapper.toResponseDto(IdentificacaoRepository.save(IdentificacaoModel));
+            IdentificacaoModel.setDescricao(identificacaoDtoComAtualizacao.getDescricao());
+            IdentificacaoModel.setObservacao(identificacaoDtoComAtualizacao.getObservacao());
+            return IdentificacaoMapper.toDto(identificacaoRepository.save(IdentificacaoModel));
         }
         return null;
+    }
+    
+    @Transactional
+    public IdentificacaoDto update(UUID id, IdentificacaoDto identificacaoDtoComAtualizacao) {
+        IdentificacaoModel IdentificacaoModel = identificacaoRepository.findByProdutoModelId(id).orElse(null);
+        if (IdentificacaoModel != null) {
+            IdentificacaoModel.setDescricao(identificacaoDtoComAtualizacao.getDescricao());
+            IdentificacaoModel.setObservacao(identificacaoDtoComAtualizacao.getObservacao());
+            return IdentificacaoMapper.toDto(identificacaoRepository.save(IdentificacaoModel));
+        }
+        return null;
+    }
+
+
+    @Transactional
+    public void delete(IdentificacaoDto identificacaoDto) {
+        identificacaoRepository.delete(IdentificacaoMapper.toModel(identificacaoDto));
     }
 
     @Transactional
     public void delete(UUID id) {
         identificacaoRepository.deleteByProdutoModelId(id);
-    }
-
-    @Transactional
-    public void delete(ProdutoResponseDto produtoResponseDto) {
-        identificacaoRepository.delete(IdentificacaoMapper.toModel(produtoResponseDto);
     }
 }
