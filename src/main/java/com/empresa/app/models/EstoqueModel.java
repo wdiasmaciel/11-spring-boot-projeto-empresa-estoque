f6ee532b71462bda20c890a66f53be6f235e95c7
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2025-04-20 17:32:52 -0300

    EstoqueDto

--- src/main/java/com/empresa/app/models/EstoqueModel.java
+++ src/main/java/com/empresa/app/models/EstoqueModel.java
@@ -1,15 +1,50 @@
 package com.empresa.app.models;
 
-import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import jakarta.validation.constraints.NotNull;
 
+import lombok.*;
+
+import java.util.UUID;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "estoque")
 public class EstoqueModel {
+
+    @EmbeddedId
+    private EstoqueModelPk id = new EstoqueModelPk();
+
     @NotNull
-	private double preco;
+    @Column(name = "preco")
+    private double preco;
+
+    @NotNull
+    @Column(name = "quantidade")
     private int quantidade;
 
+    @NotNull
+    @Column(name = "validade")
     private LocalDate validade;
 
-	
+    public void setIdProduto(UUID id_produto) {
+        this.id.setId_produto(id_produto);
+    }
+
+    public void setCnpjFilial(String cnpj_filial) {
+        this.id.setCnpj_filial(cnpj_filial);
+    }
+
+    public UUID getIdProduto() {
+        return this.id.getId_produto();
+    }
+
+    public String getCnpjFilial() {
+        return this.id.getCnpj_filial();
+    }
 }
--- src/main/java/com/empresa/app/models/EstoqueModelPK.java
+++ src/main/java/com/empresa/app/models/EstoqueModelPK.java
@@ -5,7 +5,7 @@ import jakarta.persistence.MapsId;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Embeddable;
-
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import lombok.*;
@@ -14,9 +14,10 @@ import java.util.UUID;
 
 @Data
 @Embeddable
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class EstoqueModelPK {
+public class EstoqueModelPk {
 
     @NotNull
 	@ManyToOne
@@ -27,6 +28,7 @@ public class EstoqueModelPK {
     private UUID id_produto;
 
     @NotNull
+    @NotBlank
     @ManyToOne
     @MapsId("cnpj")
     @Column(name = "cnpj_filial")
